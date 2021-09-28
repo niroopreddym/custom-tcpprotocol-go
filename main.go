1ba75bf917995a2164683fbc40fa4ac3a60ddcee
Niroop Reddy Maneti <Niroop.Maneti@carrier.com>
2021-09-28 15:45:12 +0530

    opl payload send

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/niroopreddym/custom-tcpprotocol-go/enum"
 	"github.com/niroopreddym/custom-tcpprotocol-go/mtsclient"
 )
 
@@ -18,67 +20,56 @@ func main() {
 		}
 	}()
 
-	// testEventsMap := map[int]enum.TestEvent{}
-	tcpConnect := mtsclient.NewTCPConnect("localhost", 10001, 10000)
+	testEventsMap := map[int]enum.TestEvent{}
+
+	tcpConnect := mtsclient.NewTCPConnect("127.0.0.1", 10001, 10000)
+	tcpConnect.Init()
 	defer tcpConnect.Conn.Close()
 	//do all operations on top of TLS
 	tcpConnect.WithTLS(nil)
 
 	tcpConnect.ConnectAndLogin()
 
-	// // Create some test events
-	// testEventsMap[3] = enum.SendOPLPayload
-	// testEventsMap[6] = enum.SendOPLPayload
-	// testEventsMap[9] = enum.SendOPLPayload
-	// testEventsMap[12] = enum.Exit
+	// Create some test events
+	testEventsMap[3] = enum.SendOPLPayload
+	testEventsMap[6] = enum.SendOPLPayload
+	testEventsMap[9] = enum.SendOPLPayload
+	testEventsMap[12] = enum.Exit
 
-	// // Begin example
-	// log.Println("Starting MtsClientExample in go")
+	// Begin example
+	log.Println("Starting MtsClientExample in go")
 
-	// exitTest := false
-	// for !exitTest {
-	// 	for !tcpConnect.MTSClient.Connected {
-	// 		tcpConnect.ConnectAndLogin()
-	// 		if !tcpConnect.MTSClient.Connected {
-	// 			log.Println("Trying to connect")
-	// 			time.Sleep(3000)
-	// 		} else {
-	// 			log.Println("Connected")
-	// 		}
-	// 	}
+	exitTest := false
+	for !exitTest {
+		for !tcpConnect.MTSClient.Connected {
+			// tcpConnect.ConnectAndLogin()
+			if !tcpConnect.MTSClient.Connected {
+				log.Println("Trying to connect")
+				time.Sleep(3000)
+			} else {
+				log.Println("Connected")
+			}
+		}
 
-	// 	// Send messages within this program loop
-	// 	if event, ok := testEventsMap[TestElapsedSeconds]; ok {
-	// 		//do something here
-	// 		switch event {
-	// 		case enum.SendOPLPayload:
-	// 			tcpConnect.SendTestOPLPayload(nil)
-	// 			break
-	// 		case enum.Exit:
-	// 			exitTest = true
-	// 			break
-	// 		}
-	// 	}
+		// Send messages within this program loop
+		if event, ok := testEventsMap[TestElapsedSeconds]; ok {
+			//do something here
+			switch event {
+			case enum.SendOPLPayload:
+				tcpConnect.SendTestOPLPayload(nil)
+				break
+			case enum.Exit:
+				exitTest = true
+				break
+			}
+		}
 
-	// 	if !exitTest {
-	// 		time.Sleep(1000)
-	// 		TestElapsedSeconds++
-	// 	}
-	// }
+		if !exitTest {
+			time.Sleep(1000)
+			TestElapsedSeconds++
+		}
+	}
 
 	// fmt.Println(tcpConnect)
 	log.Println("Ending MtsClientExample go")
 }
-
-// func main() {
-// 	data := `	 �{"version":1,"attributeRoute":"A","route":9,"srcId":1,"dstId":2,"rpcId":1,"reply":false,"error":false,"jwt":null,"data":"AAAAAA=="}`
-// 	byteArr, err := json.Marshal(data)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	mtsResponseMessage := model.MTSMessage{}
-// 	err = json.Unmarshal(byteArr, &mtsResponseMessage)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
