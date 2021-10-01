6941d13aeb2f74eaa8b16c64feb2d0d7dea9877c
Niroop Reddy Maneti <Niroop.Maneti@carrier.com>
2021-10-01 15:46:23 +0530

    helpers cleaned

--- helpers/helper.go
+++ helpers/helper.go
@@ -3,7 +3,6 @@ package helper
 import (
 	"crypto/tls"
 	"encoding/binary"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -123,37 +122,4 @@ func GetConnection(connectionString string) (net.Conn, error) {
 	}
 
 	return conn, err
-}
-
-//CreateResponse creates a MTSMessage as Response
-func CreateResponse(requestMessage *model.MTSMessage, responseType enum.MTSRequest, attrRoute *string, isError bool, jwt *string, data []byte) model.MTSMessage {
-	mtsMessage := model.MTSMessage{
-		Version:        1,
-		Route:          responseType,
-		SrcID:          requestMessage.SrcID,
-		DstID:          requestMessage.DstID,
-		RPCID:          requestMessage.RPCID,
-		Reply:          true,
-		IsError:        isError,
-		Data:           data,
-		AttributeRoute: attrRoute,
-		JWT:            jwt,
-	}
-
-	return mtsMessage
-}
-
-//CreateErrorResponse error response
-func CreateErrorResponse(errorID enum.MtsErrorID, errorMsg string, requestMessage *model.MTSMessage, responseType enum.MTSRequest, attrRoute *string, jwt *string) model.MTSMessage {
-	var errorResponseData = model.MtsErrorResponse{
-		MtsError:        errorID,
-		MtsErrorMessage: errorMsg,
-	}
-
-	errorResponseByteArray, err := json.Marshal(errorResponseData)
-	if err != nil {
-		fmt.Println("Error getting the client cert", err)
-	}
-
-	return CreateResponse(requestMessage, responseType, attrRoute, true, jwt, errorResponseByteArray)
 }
